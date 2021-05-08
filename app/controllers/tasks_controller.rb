5b7a129f8b171699c92cd63d21f94e08bf8bd6a7
大津　秀介 <otsu0412@yahoo.co.jp>
2021-05-08 16:58:41 +0900

    削除実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -25,6 +25,11 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+  end
+
   private
   def task_params
    params.require(:task).permit(:title, :content)
